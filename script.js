e7f78f255b0195dce9deb16354157a0b1a9a7ee3
Mateus <mateusribeirodaniel@hotmail.com>
2023-01-21 15:47:55 -0300

    animacao ao scroll

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 //Scroll Suave Links Internos
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
   });
 }
+initScrollSuave();
 
-linksInternos.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
+//Animaçao ao Scroll
+const sections = document.querySelectorAll(".js-scroll");
+
+function initAnimacaoScroll() {
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.65;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimacaoScroll();
